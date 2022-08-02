507af2301e195477b7ad87b5ccb5cb606b373545
Heber Ramirez <heber96nr@gmail.com>
2022-08-02 16:54:56 -0300

    add login, registro, inicio con navbar y footer

--- Instituto/usuario/urls.py
+++ Instituto/usuario/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views import View
 from . import views
+
 
 urlpatterns = [
     path('registro/', views.Registro.as_view(), name='registro'),
-
-
+    
+    
 ]
