db57a902ced8cb4e11e6cd4cda095a7aa83d10d3
federicos1 <fedes7777@gmail>
2022-08-03 23:06:51 -0300

    a

--- Instituto/blog/admin.py
+++ Instituto/blog/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from .models import Post
-<<<<<<< HEAD
 # Register your models here.
 
 
 admin.site.register(Post)
-=======
-
-admin.site.register(Post)
->>>>>>> 27a1904f7c5141aa32d63c786b58b22b358088a5
