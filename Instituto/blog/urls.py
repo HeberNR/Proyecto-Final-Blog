ce9fad7f0f9c57f2fd8988145fbbf1f070cdb37d
federicos1 <fedes7777@gmail>
2022-08-07 17:48:12 -0300

    se agrego la funcion borrar post

--- Instituto/blog/urls.py
+++ Instituto/blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('inicio/', views.Inicio.as_view(template_name="inicio.html"), name='inicio'),
     path('crear/', views.CrearPost.as_view(), name='crear'),
     path('post/<int:pk>/',views.BlogDetail.as_view(template_name = 'details.html'),name= 'details'),
-    path('edit/post/<int:pk>',views.PostUpdate.as_view(template_name = 'edit.html'),name='editar'),
-    path('edit/delete/<int:pk>',views.PostDelete.as_view(),name='borrar'),
+    path('post/edit/<int:pk>',views.PostUpdate.as_view(template_name = 'edit.html'),name='editar'),
+    path('post/delete/<int:pk>',views.PostDelete.as_view(template_name = 'post_confirm_delete.html'),name='borrar'),
     
  ]
