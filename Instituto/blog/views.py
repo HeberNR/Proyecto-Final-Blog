c8fc6eca4c968e4a6d4a536241e53333dd251ef0
federicos1 <fedes7777@gmail>
2022-08-07 18:32:07 -0300

    ya se puede comentar post por fin

--- Instituto/blog/views.py
+++ Instituto/blog/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.views.generic import DetailView,ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Comment
@@ -7,6 +7,7 @@ from core.mixins import StaffRequiredMixin
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from .forms import CommentPost, PostForm
 from django.urls import reverse, reverse_lazy
+
 
 
 # Create your views here.
@@ -53,28 +54,28 @@ class BlogDetail(DetailView):
     model = Post
     template_name: 'details.html'
     form_class = CommentPost
-
+    form = CommentPost
     def get_context_data(self, **kwargs):
         
         context = super().get_context_data(**kwargs)
-        context['form'] = self.form_class
+        context['form'] = self.form
         context['comments'] = self.object.comments.all()
         return context
 
-    # def form_valid(self, form):
-    #     form.instance.author_id = self.request.user.id
-    #     return super().form_valid(form)
 
-    # def post(self, request, *args, **kwargs):
-    #     self.object = self.get_object()
-    #     form = self.get_form()
-    #     if form.is_valid():
-    #         return self.form_valid(form)
-    #     else:
-    #         return self.form_invalid(form)
+    def post(self, request, *args, **kwargs):
+        form = CommentPost(request.POST)
+        if form.is_valid():
+            post = self.get_object()
+            form.instance.author = self.request.user
+            form.instance.posts = post
+            form.save()
+            return redirect(reverse('details', kwargs={'pk': post.id}))    
+        else:
+            return self.form_invalid(form)
 
-    # def get_success_url(self):
-    #     return reverse('details', kwargs = {'pk': self.object.id})
+    def get_success_url(self):
+        return reverse('details', kwargs = {'pk': self.object.id})
     
 
 
@@ -82,29 +83,6 @@ class BlogDetail(DetailView):
 
 
 
-
-# def post_detail(request,pk):
-#     template_name = 'details.html'
-#     post = get_object_or_404(Post, pk=pk)
-#     comments = post.post_comentario.all()
-#     new_comment = None
-
-    
-
-#     if request.method == 'POST':
-#         form = CommentPost(request.POST)
-#         if form.is_valid():
-#             new_comment = form.save(commit=False)
-#             new_comment.post = post
-#             new_comment.save()
-#     else:
-#         form = CommentPost()
-
-#     return render(request, template_name, {'post': post,
-#                                            'comments': comments,
-#                                            'new_comment': new_comment,
-#                                            'comment_form': form,
-#                                            })
 
 
     
@@ -121,10 +99,6 @@ class PostUpdate(StaffRequiredMixin,UpdateView):
         
         return reverse('inicio')
     
-    # def form_valid(self, form):
-    #     pk = self.kwargs.get('pk')
-    #     obj = get_object_or_404(Post, pk=pk)
-
 
 class PostDelete(DeleteView,LoginRequiredMixin,StaffRequiredMixin):
     model = Post
