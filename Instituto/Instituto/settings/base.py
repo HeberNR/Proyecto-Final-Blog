43cedbbde2c49661cad4f55dd3608e5b9c7f5772
federicos1 <fedes7777@gmail>
2022-08-25 00:27:53 -0300

    naturaltime en comentarios

--- Instituto/Instituto/settings/base.py
+++ Instituto/Instituto/settings/base.py
@@ -24,6 +24,7 @@ AUTH_USER_MODEL = 'usuario.Usuario'
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.humanize',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
